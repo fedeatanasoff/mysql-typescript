197c4f23b18b694cb677688835d8b15c455768ca
fedeatanasoff <fede.atanasoff@gmail.com>
2019-06-10 00:09:24 -0300

    clase para mysql

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import Server from "./server/server";
 import router from "./router/router";
+import MySQL from "./mysql/mysql";
 
 const server = Server.init(3500);
 server.app.use(router);
+
+const mysql = new MySQL();
 
 server.start(() => {
   console.log("Server on: 3500");
---
+++ src/mysql/mysql.ts
@@ -0,0 +1,32 @@
+import mysql = require("mysql");
+
+export default class MySQL {
+  private static _instance: MySQL;
+
+  cnn: mysql.Connection;
+  conectado: boolean = false;
+
+  constructor() {
+    console.log("Clase Mysql inicializada");
+    this.cnn = mysql.createConnection({
+      host: "localhost",
+      user: "node_user",
+      password: "123456",
+      database: "node_db"
+    });
+
+    this.conectarDB();
+  }
+
+  private conectarDB() {
+    this.cnn.connect((err: mysql.MysqlError) => {
+      if (err) {
+        console.log(err.message);
+        return;
+      }
+
+      this.conectado = true;
+      console.log("Conexion a BD exitosa");
+    });
+  }
+}
